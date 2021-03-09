7bdb86db4da0ce82df1de6945e1e870ea87f4d64
luzhenxiang <59001164+luzhenxiang@users.noreply.github.com>
2021-03-09 16:49:03 +0800

    店铺管理

--- src/main/java/com/newer/service/StoreService.java
+++ src/main/java/com/newer/service/StoreService.java
@@ -1,5 +1,21 @@
 package com.newer.service;
 
-public interface StoreService {
+import java.util.List;
 
+import com.newer.bean.Store;
+
+public interface StoreService {
+	public List<Store> selStore();
+	
+	public List<Store> noPassStore();
+	
+	public Boolean delStore(int stoid);
+	
+	public Store detStore(int stoid);
+	
+	public List<Store> fuzzyQuery(String n);
+
+	public Boolean upStore(Store store);
+	
+	public Boolean refuseStore(Store store);
 }
--- src/main/java/com/newer/service/impl/StoreServiceImpl.java
+++ src/main/java/com/newer/service/impl/StoreServiceImpl.java
@@ -1,7 +1,62 @@
 package com.newer.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.newer.bean.Store;
+import com.newer.dao.StoreDao;
 import com.newer.service.StoreService;
-
+@Service
 public class StoreServiceImpl implements StoreService{
+	@Autowired
+	StoreDao storeDao;
+	
+	
+	@Override
+	public List<Store> selStore() {
+		
+		return storeDao.selstore();
+	}
 
+
+	@Override
+	public List<Store> noPassStore() {
+		return storeDao.noPassStore();
+	}
+
+
+	@Override
+	public Boolean delStore(int stoid) {
+		return storeDao.delstore(stoid)>0?true:false;
+	}
+
+
+	@Override
+	public Store detStore(int stoid) {
+		return storeDao.detstore(stoid);
+	}
+
+
+	@Override
+	public List<Store> fuzzyQuery(String n) {
+		return storeDao.fuzzyquery(n);
+	}
+
+
+	@Override
+	public Boolean upStore(Store store) {
+		return storeDao.upstore(store)>0?true:false;
+	}
+
+
+	@Override
+	public Boolean refuseStore(Store store) {
+		
+		return storeDao.refusestore(store)>0?true:false;
+	}
+
+
+	
 }
